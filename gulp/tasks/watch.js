709b03954ea5028369710faef8a991e5f9090c74
Kevin <omallek@gmail.com>
2019-03-03 18:02:18 -0600

    css focus on wrapper headlines rows and more

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+   browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+});
+
+// second argument are dependencies
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
