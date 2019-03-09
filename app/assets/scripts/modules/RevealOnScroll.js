b550e475079f2ba31e54f5599e5884ec86091fe6
Kevin <omallek@gmail.com>
2019-03-09 14:10:52 -0600

    revealonscroll class function completed

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offset = offset;
+    this.createWaypoints();
+    this.hideInitially();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createWaypoints() {
+    const that = this;
+
+    this.itemsToReveal.each(function() {
+      const currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass('reveal-item--is-visible');
+        },
+        offset: that.offset,
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
