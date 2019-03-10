078a782f0cfac3fd6eef67e0dded898238d9a3ee
Kevin <omallek@gmail.com>
2019-03-10 06:59:59 -0500

    adds sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,12 @@
+import $ from 'jquery';
+
+// MODULE IMPORTS //
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
+const stickyHeader = new StickyHeader();
